2a802182e8eac1aae158eafe2aa16dc6f14b27ef
Leo <leo.wei@alumni.ubc.ca>
2020-12-29 15:09:47 -0700

    added header component to everypage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import React from 'react';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 import {Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component = {HomePage}/>
         <Route path='/shop' component = {ShopPage}/>
