22c6b404c88f40c85eb64d1d5ba31e3117cd355d
Leo <leo.wei@alumni.ubc.ca>
2020-12-30 17:40:14 -0700

    implemented firebase signup, storing authenticated users into database

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 import {Switch, Route} from 'react-router-dom';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component{
   constructor(){
@@ -22,10 +22,29 @@ class App extends React.Component{
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
-      console.log(user);
-    })
+
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
+      if (userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot =>{
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, ()=>{
+            console.log(this.state)
+          }
+          )
+        })
+        
+      }
+      else{
+        this.setState({currentUser:userAuth});
+      }
+      
+    });
   }
 
   componentWillUnmount(){
