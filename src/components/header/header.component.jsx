a7b4a485fa74e2415bd49fa3c8cc6b4175f0d1be
Leo <leo.wei@alumni.ubc.ca>
2021-01-01 15:05:08 -0700

    added cartdropdown component with toggle hidden state with cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,12 +5,13 @@ import {connect} from 'react-redux';
 
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropDown from '../cart-dropdown/cart-dropdown.component';
 
 import {auth} from '../../firebase/firebase.utils';
 
 import './header.styles.scss';
 
-const Header = ({currentUser}) =>(
+const Header = ({currentUser, hidden}) =>(
     <div className = 'header'>
         <Link className = 'logo-container' to="/">
             <Logo className = 'logo'/>
@@ -31,14 +32,19 @@ const Header = ({currentUser}) =>(
             }
             <CartIcon/>
         </div>
+        {
+            hidden? null:<CartDropDown/>
+        }
+        
 
     </div>
 
 
 )
 
-const mapStateToProps = state =>({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user:{currentUser}, cart: {hidden}}) =>({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
