7f218d3b23aea393c6aa80d2a713f96031cd2039
Leo <leo.wei@alumni.ubc.ca>
2021-01-14 19:58:37 -0700

    added spinner for while shop pages are fetching data from firebase

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -17,5 +17,5 @@ export const selectCollectionsForPreview = createSelector(
 export const selectCollection = memoize((collectionUrlParam) =>
     createSelector(
         [selectCollections],
-        collections => collections[collectionUrlParam]
+        collections => (collections ? collections[collectionUrlParam] : null)
 ));
