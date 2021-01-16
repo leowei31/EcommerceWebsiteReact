2cd3c8a10512d3f4e8e5d16db369d7c06ee237db
Leo <leo.wei@alumni.ubc.ca>
2021-01-16 16:10:27 -0700

    separated shop collections overview and collections into its own indivdual container to avoid props tunnelling

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -2,13 +2,18 @@ import ShopActionTypes from './shop.types';
 
 import {firestore, convertCollectionsSnapshotToMap} from '../../firebase/firebase.utils';
 
-export const fetchCollectionsStart = (collectionsMap) => ({
+export const fetchCollectionsStart = collectionsMap => ({
   type: ShopActionTypes.FETCH_COLLECTIONS_START
 });
 
 export const fetchCollectionsSuccess = collectionsMap => ({
   type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
   payload: collectionsMap
+})
+
+export const fetchCollectionsFailure = errorMessage => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage
 })
 
 export const fetchCollectionsStartAsync = () => {
@@ -19,7 +24,7 @@ export const fetchCollectionsStartAsync = () => {
     collectionRef.get().then((snapshot) => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       dispatch(fetchCollectionsSuccess(collectionsMap));
-    });
+    }).catch(error => dispatch(fetchCollectionsFailure(error.message)));
   }
 
 }
