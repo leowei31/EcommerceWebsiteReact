2a22871a94de482980072ec42dd30dcfd99a937a
Leo <leo.wei@alumni.ubc.ca>
2021-01-18 15:47:40 -0700

    added redux saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import {createStore, applyMiddleware} from 'redux';
 import {persistStore} from 'redux-persist';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk'
+import thunk from 'redux-thunk';
+import createSageMiddleware from 'redux-saga';
 
 import rootReducer from './root-reducer';
 
+const sagaMiddleware = createSageMiddleware();
 const middlewares = [thunk];
 
 if (process.env.NODE_ENV === 'development'){
