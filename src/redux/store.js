2a5d1ff39302e9bcd6e4646d8ae3f3c6330394e3
Leo <leo.wei@alumni.ubc.ca>
2021-01-18 16:50:24 -0700

    replaced thunk middleware with redux sagas

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,22 @@
 import {createStore, applyMiddleware} from 'redux';
 import {persistStore} from 'redux-persist';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
 import createSageMiddleware from 'redux-saga';
 
 import rootReducer from './root-reducer';
 
+import {fetchCollectionsStart} from './shop/shop.sagas';
+
 const sagaMiddleware = createSageMiddleware();
-const middlewares = [thunk];
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === 'development'){
     middlewares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(fetchCollectionsStart);
 
 export const persistor = persistStore(store);
 
