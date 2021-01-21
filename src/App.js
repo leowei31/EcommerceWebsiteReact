8296c94dea89be67645b202f717b352ca758164b
Leo <leo.wei@alumni.ubc.ca>
2021-01-20 22:20:52 -0700

    refactored user sign in process with sagas, need to work on user session persistence

--- src/App.js
+++ src/App.js
@@ -11,9 +11,7 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import CheckoutPage from './pages/checkout/checkout.component';
 
 import {Switch, Route, Redirect} from 'react-router-dom';
-import {auth, createUserProfileDocument } from './firebase/firebase.utils';
 
-import {setCurrentUser} from './redux/user/user.actions';
 import {selectCurrentUser} from './redux/user/user.selectors';
 
 
@@ -23,27 +21,26 @@ class App extends React.Component{
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    const {setCurrentUser} = this.props;
-    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
-      if (userAuth){
-        const userRef = await createUserProfileDocument(userAuth);
+  //   this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
+  //     if (userAuth){
+  //       const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot(snapShot =>{
-          setCurrentUser({
-              id: snapShot.id,
-              ...snapShot.data()
-          })
-        })
+  //       userRef.onSnapshot(snapShot =>{
+  //         setCurrentUser({
+  //             id: snapShot.id,
+  //             ...snapShot.data()
+  //         })
+  //       })
         
-      }
+  //     }
 
-      setCurrentUser(userAuth);
+  //     setCurrentUser(userAuth);
       
-    });
-  }
+  //   });
+  // }
 
-  componentWillUnmount(){
-    this.unsubscribeFromAuth();
+  // componentWillUnmount(){
+  //   this.unsubscribeFromAuth();
   }
 
   render(){
@@ -66,8 +63,5 @@ const mapStateToProps = createStructuredSelector({
 })
 
 
-const mapDispatchToProps = dispatch =>({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-})
 
-export default connect(mapStateToProps,mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
