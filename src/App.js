210b4fc35e12257810fd121fab31df8d63915164
Leo <leo.wei@alumni.ubc.ca>
2021-01-23 20:55:38 -0700

    converted shop component and app js to use hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
 
@@ -16,32 +16,23 @@ import {selectCurrentUser} from './redux/user/user.selectors';
 
 import {checkUserSession} from './redux/user/user.actions';
 
-class App extends React.Component{
-
-  unsubscribeFromAuth = null;
-
-  componentDidMount(){
-    const {checkUserSession} = this.props;
+const App = ({checkUserSession, currentUser}) => {
+  useEffect(()=>{
     checkUserSession()
-  }
-  
-  componentWillUnmount(){
-    this.unsubscribeFromAuth();
-  }
+  }, [checkUserSession]);
 
-  render(){
-    return (
-      <div>
-        <Header/>
-        <Switch>
-          <Route exact path='/' component = {HomePage}/>
-          <Route path='/shop' component = {ShopPage}/>
-          <Route exact path='/signin' render={()=> this.props.currentUser? (<Redirect to = '/'/>) : (<SignInAndSignUpPage/>)}/>
-          <Route exact path='/checkout' component = {CheckoutPage}/>
-        </Switch>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Header/>
+      <Switch>
+        <Route exact path='/' component = {HomePage}/>
+        <Route path='/shop' component = {ShopPage}/>
+        <Route exact path='/signin' render={()=> currentUser? (<Redirect to = '/'/>) : (<SignInAndSignUpPage/>)}/>
+        <Route exact path='/checkout' component = {CheckoutPage}/>
+      </Switch>
+    </div>
+  );
+
 }
 
 const mapStateToProps = createStructuredSelector({
