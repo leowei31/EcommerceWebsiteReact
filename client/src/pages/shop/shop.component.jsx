9b448d1bdfcf6548101e6e33476e419cbeee8d86
Leo <leo.wei@alumni.ubc.ca>
2021-02-02 14:18:59 -0700

    Added spinner component and used react lazy and suspense to chunk

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,11 +1,12 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, lazy, Suspense} from 'react';
 import {Route} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {fetchCollectionsStart} from '../../redux/shop/shop.actions';
 
-import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
-import CollectionPageContainer from '../collection/collection.container';
+import Spinner from '../../components/spinner/spinner.component';
 
+const CollectionsOverviewContainer = lazy(()=> import('../../components/collections-overview/collections-overview.component'));
+const CollectionPageContainer = lazy(()=> import('../../pages/collection/collection.container'));
 
 const ShopPage= ({fetchCollectionsStart, match}) => {
     useEffect(()=>{
@@ -14,6 +15,7 @@ const ShopPage= ({fetchCollectionsStart, match}) => {
 
     return(
     <div className = 'shop-page'>
+        <Suspense fallback = {<Spinner/>}>
             <Route 
                 exact path = {`${match.path}`} 
                 component = {CollectionsOverviewContainer}
@@ -21,7 +23,9 @@ const ShopPage= ({fetchCollectionsStart, match}) => {
             <Route 
                 path = {`${match.path}/:collectionId`} 
                 component = {CollectionPageContainer}
-            />
+            />        
+        </Suspense>
+
     </div>
     );
     
