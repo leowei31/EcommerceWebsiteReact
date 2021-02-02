2313a5dc8454fff64b56322f09c5eaa8917825ad
Leo <leo.wei@alumni.ubc.ca>
2021-02-01 21:26:17 -0700

    added global styles js file to replace app css

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,3 @@
-import './App.css';
-
 import React, {useEffect} from 'react';
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
@@ -16,6 +14,8 @@ import {selectCurrentUser} from './redux/user/user.selectors';
 
 import {checkUserSession} from './redux/user/user.actions';
 
+import {GlobalStyle} from './global.styles';
+
 const App = ({checkUserSession, currentUser}) => {
   useEffect(()=>{
     checkUserSession()
@@ -23,6 +23,7 @@ const App = ({checkUserSession, currentUser}) => {
 
   return (
     <div>
+      <GlobalStyle/>
       <Header/>
       <Switch>
         <Route exact path='/' component = {HomePage}/>
